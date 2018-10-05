ea89354c6681b8b1d032edd52727d017cd74aea5
Kévin Lejeune <lejeune-kevin@hotmail.fr>
2018-10-05 13:37:26 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function formulaire() {
+  var nom = document.getElementById("lastname").value;
+  var prenom = document.getElementById("firstname").value;
+  var ville = document.getElementById("city").value;
+
+  alert("Nom : " + nom + "\nPrénom : " + prenom + "\nVille : " + ville);
+}
